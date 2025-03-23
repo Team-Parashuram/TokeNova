bd3d2f0f5fb283b4335db9b4ebc4e558d8643a0b
Harsh Soni <52278293+harshsoni-harsh@users.noreply.github.com>
2025-03-23 16:22:27 +0530

    hardhat config add sepolia

--- web3-contracts/hardhat.config.ts
+++ web3-contracts/hardhat.config.ts
@@ -1,9 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ignition-ethers";
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const API_URL = process.env.API_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  defaultNetwork: "localhost",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
 };
 
 export default config;
